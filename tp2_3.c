a86b7f2e7dfb61be7ee9f5ed4a744fa304f1e150
NazaretCS <nazaretcampos27@gmail.com>
2022-06-20 07:49:35 -0300

    agregado del archivo tp2_3.c

---
+++ tp2_3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define N 5
+#define M 7
+
+int main(void){
+
+    int i,j;
+    float mt[N][M];
+    float *PuntMT;
+    PuntMT = &mt[0][0];
+
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++){
+        for(j = 0;j<M; j++){
+            *(PuntMT + (i * 7 + j))=1+rand()%100;
+            printf("%.2f ", *(PuntMT + (i * 7 + j)));
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+
+
+...
+
+
+*/
